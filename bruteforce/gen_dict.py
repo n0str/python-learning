ddb197e04ae490489ce5df9bbb0a274bf7f3ed7b
n0str <ntpcp@yandex.ru>
2014-05-04 21:32:26 +0800

    Converted tabs to spaces according to the github feature (bug, i suppose).

--- bruteforce/gen_dict.py
+++ bruteforce/gen_dict.py
@@ -2,13 +2,13 @@ import itertools
 from string import ascii_lowercase
 
 def bruteforce(alphabet, maxlength):
-	return (''.join(candidate)
-		for candidate in itertools.chain.from_iterable(itertools.product(alphabet, repeat=i)
-		for i in range(1, maxlength + 1)))
+    return (''.join(candidate)
+        for candidate in itertools.chain.from_iterable(itertools.product(alphabet, repeat=i)
+        for i in range(1, maxlength + 1)))
 
 if __name__ == "__main__":
-	alphabet = ascii_lowercase
-	count = 0
-	for attempt in bruteforce(alphabet, 5):
-		count += 1
-	print count
+    alphabet = ascii_lowercase
+    count = 0
+    for attempt in bruteforce(alphabet, 5):
+        count += 1
+    print count
